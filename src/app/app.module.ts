e0642575077795e960549849ee0de81b9b376f8f
Shravil09 <93884237+Shravil09@users.noreply.github.com>
2022-04-24 20:30:57 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
+import productsEffects from './effects/products.effect';
+import { productReducer } from './reducer/products.reducer';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    EffectsModule.forRoot([productsEffects]),
+    StoreModule.forRoot({"products":productReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
